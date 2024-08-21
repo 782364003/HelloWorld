eaa3e5f670b7b20e5347c7c1181754377c6ce656
lls <782364003@qq.com>
2024-08-21 16:47:30 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    @ResponseStatus(HttpStatus.OK)
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
